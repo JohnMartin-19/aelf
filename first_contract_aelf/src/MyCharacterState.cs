7134b9f24774f093226615aa5ab560d72d2f9689
John <johnnkonge2020@gmail.com>
2024-03-02 22:35:44 +0300

    added  base folder

--- first_contract_aelf/src/MyCharacterState.cs
+++ first_contract_aelf/src/MyCharacterState.cs
@@ -1,11 +1,19 @@
+using System.Diagnostics.CodeAnalysis;
 using AElf.Sdk.CSharp.State;
+using AElf.Standards.ACS6;
+using AElf.Types;
 
 namespace AElf.Contracts.MyCharacter
 {
     // The state class is access the blockchain state
     public class MyCharacterState : ContractState 
     {
-        // A state that holds string value
-        public StringState Message { get; set; }
+       //create a storage space for Character
+        public BoolState Initialized { get; set; }
+        public MappedState<Address, Character> Characters { get; set; }
+
+        //encapsulate AEDPoS consensus contract reference state
+        internal RandomNumberProvideacsrContractContainer.RandomNumberProvideacsrContractReferenceState
+            RandomNumberContract { get; set; }
     }
 }
